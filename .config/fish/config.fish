f4ed7a462da302c9b747d0c7d63af7c121aeb63d
Florian Sanders <sanders.florian@gmail.com>
2024-09-02 21:12:18 +0200

    feat(gnome-keyring): setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,12 @@ end
 #         source ~/.keychain/$HOSTNAME-fish
 #     end
 
-# Start or re-use a gpg-agent.
-gpgconf --launch gpg-agent
-
-# Ensure that GPG Agent is used as the SSH agent
-set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh end
+# # Start or re-use a gpg-agent.
+# gpgconf --launch gpg-agent
+#
+# # Ensure that GPG Agent is used as the SSH agent
+# set -e SSH_AUTH_SOCK
+# set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh end
 
 export EDITOR='nvim'
 
--- .profile
+++ .profile
@@ -1,3 +1,4 @@
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
 export EDITOR="nvim"
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
